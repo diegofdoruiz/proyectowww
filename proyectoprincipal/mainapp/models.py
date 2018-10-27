e84580d8391242103841c9573d529ea0ca7202d3
diegopccali1 <dieg.fernando.ruiz@correounivalle.edu.co>
2018-10-26 20:58:54 -0500

    Permisos en auth_permission

--- proyectoprincipal/mainapp/models.py
+++ proyectoprincipal/mainapp/models.py
@@ -1,13 +1,6 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Permission
 from django.db import models
 from django.utils.translation import gettext as _
-
-
-class Permission(models.Model):
-    name = models.CharField(max_length=100, null=False)
-
-    def __str__(self):
-        return self.name
 
 
 class Rol(models.Model):
