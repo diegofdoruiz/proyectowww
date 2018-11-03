3cc4915c5bf7647b544f6fad64d5eab81d057539
diegopccali1 <dieg.fernando.ruiz@correounivalle.edu.co>
2018-11-02 20:20:01 -0500

    Mejoras en formularios y vistas de usuarios

--- proyectoprincipal/mainapp/urls.py
+++ proyectoprincipal/mainapp/urls.py
@@ -4,10 +4,10 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('signup_alternative/', views.SignUpViewAlternative.as_view(), name='signup_alternative'),
-    # path('editprofile/<int:pk>/', views.edit_profile, name='edit_profile'),
+    path('user_list/', views.user_list, name='user_list'),
+    path('user_edit/<int:pk>/', views.user_edit, name='user_edit'),
     path('edit_profile/', views.edit_profile, name='edit_profile'),
     path('create_role/', views.create_role, name='create_role'),
-    path('signup/', views.signup, name='signup'),
+    path('signup/', views.register, name='signup'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
